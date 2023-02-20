4bbfeb1c0b6800eb4062d3668dafffdf3f098b0a
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2023-02-19 20:54:50 -0800

    commit

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,17 +1,24 @@
-let randomNum = Math.round(Math.random())
-let choice = prompt('Select Heads or Tails');
+let coinFlip = Math.round(Math.random());
+let choice = prompt('Select heads (h) or tails (t)');
 
-if (choice < coinFlip){
-    alert('The flip was heads and you chose heads...you win!');
-} else if (choice > coinFlip) {
-    alert('The flip was heads but you chose tails...you lose!');
-} else if ( {
-    The flip was tails but you chose heads...you lose!
-    The flip was tails and you chose tails...you win!
+if (coinFlip < .05) {
+    coinFlip = 'heads';
 } else {
-    alert('invalid input');
+    coinFlip = 'tails';
 }
 
-
-
-
+if (choice === 'h') {
+    if (coinFlip === 'heads') {
+        alert('The flip was heads and you chose heads...you win!');
+    } else if (coinFlip === 'tails') {
+        alert('The flip was tails but you chose heads...you lose!');
+    }
+} else if (choice === 't') {
+    if (coinFlip === 'heads') {
+        alert('The flip was heads but you chose tails...you lose!');
+    } else if (coinFlip === 'tails') {
+        alert('The flip was tails and you chose tails...you win!');
+    }
+} else {
+    alert('Your selection is invalid.');
+}
